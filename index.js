ef0489f8aed45edd509e03a809d071898c5ea6d4
Erica Janine Sanguir <ericajanisan@gmail.com>
2018-10-17 05:17:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
-function theBeatlesPlay(musican,instrument){
-  var array= [];
-  for(var i=0;i<4;i++){
-    return
+var musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
+var instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
+
+
+theBeatlesPlay(musicians, instruments); 
+
+function theBeatlesPlay(musicians, instruments) {
+  var newArr = []; 
+  for(var i =0; i < musicians.length; i++) {
+    newArr.push(musicians[i] + " plays " + instruments[i]);
   }
+  return newArr;
 }
