a0c29f80f67db2e92337125c3c46bd495032ac81
Erica Janine Sanguir <ericajanisan@gmail.com>
2018-10-17 04:58:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+/*const musicians = ["John", "Paul", "George", "Ringo"];
+
+const instruments = ["guitar", "guitar", "bass", "drums"];
+
 function theBeatlesPlay(musicians, instruments) {
   var array = [];
   for (let i = 0; i < 4; i++) {
@@ -7,3 +11,30 @@ function theBeatlesPlay(musicians, instruments) {
 }
 
 theBeatlesPlay(musicians, instruments);
+
+/*const facts = [
+  "He was the last Beatle to learn to drive",
+  "He was never a vegetarian",
+  "He was a choir boy and boy scout",
+  "He hated the sound of his own voice"
+];
+
+function johnLennonFacts(facts) {
+  while () {
+    facts[n] + "!!!";
+  }
+  return facts;  
+}*/
+
+function iLoveTheBeatles(x) {
+  var array = [];
+  do {
+    x = array.push("I love the Beatles!");
+    x++;
+  } while (x < 15); {
+    
+  }
+  return array;
+}
+
+//iLoveTheBeatles(12);
