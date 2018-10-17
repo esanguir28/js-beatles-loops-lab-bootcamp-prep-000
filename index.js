7ff1c0f779bcddcd760386c56026e4bea0526529
Erica Janine Sanguir <ericajanisan@gmail.com>
2018-10-17 05:21:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -25,10 +25,8 @@ function johnLennonFacts(arr) {
  johnLennonFacts(facts); 
  
  function iLoveTheBeatles(number) { 
-  
-let newArray = []; 
-  do {
-  newArray.push(`I love the Beatles!`);
+  let newArray = []; 
+  do {newArray.push(`I love the Beatles!`);
   number++;
   } while (number < 15);
   return newArray;
